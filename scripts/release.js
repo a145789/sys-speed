5bc2758e01fab0d6b38031a3532318d9c9486b5f
a145789 <2091927351@qq.com>
2024-07-14 17:11:25 +0800

    chore: release

--- scripts/release.js
+++ scripts/release.js
@@ -1,13 +1,16 @@
-import fs from 'node:fs'
-import { release } from '@varlet/release'
+import fs from "node:fs"
+import { release } from "@varlet/release"
 
 async function task() {
- const version =  fs.readFileSync('package.json', 'utf-8').toJSON().version
+  const version = fs.readFileSync("package.json").toJSON().version
 
- const tauriCOnfig = fs.readFileSync('../src-tauri/tauri.conf.json', 'utf-8').toJSON()
+  const tauriCOnfig = fs.readFileSync("src-tauri/tauri.conf.json").toJSON()
 
- tauriCOnfig.version = version
- fs.writeFileSync('../src-tauri/tauri.conf.json', JSON.stringify(tauriCOnfig, null, 2))
+  tauriCOnfig.version = version
+  fs.writeFileSync(
+    "src-tauri/tauri.conf.json",
+    JSON.stringify(tauriCOnfig, null, 2),
+  )
 }
 
 release({ task, skipNpmPublish: true })
